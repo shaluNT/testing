b5004bb0ec88bb7f1af65fbd30e584a8de4bdd6f
Shalu_S <61003791+shaluNT@users.noreply.github.com>
2020-07-28 01:36:05 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,6 @@
 package com.mycompany.app;
+import org.openqa.selenium.Webdriver;
+import org.openqa.selenium.chrome.Chromedriver;
 
 /**
  * Hello world!
@@ -9,5 +11,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver","/home/nt/code/jenkins/chromedriver");
+        Webdriver driver = new Chromedriver();
+        driver.get("https://www.javatpoint.com");
+        System.out.println(driver.getTitle());
+        
     }
 }
