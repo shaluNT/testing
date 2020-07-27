0c2270cfe668c189baeebfe0d73f5dfe5149cac2
Shalu_S <61003791+shaluNT@users.noreply.github.com>
2020-07-28 02:11:40 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,11 +4,18 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    
+    
+    
     /**
      * Rigorous Test :-)
      */
