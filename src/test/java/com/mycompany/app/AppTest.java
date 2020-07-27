4d973f9232ed4d8354e181891b055048968e6276
Shalu_S <61003791+shaluNT@users.noreply.github.com>
2020-07-28 02:12:43 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -14,7 +14,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class AppTest 
 {
     
-    
+     System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver","/home/nt/code/jenkins/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.javatpoint.com");
+System.out.println(driver.getTitle());
     
     /**
      * Rigorous Test :-)
